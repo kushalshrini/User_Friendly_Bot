ff50c7a141bf800fa4a0da1ac7d187f8a036e0b1
kushalshrini <42143431+kushalshrini@users.noreply.github.com>
2021-04-15 20:33:07 +0530

    Update training.py

--- user_friendly_bot/training.py
+++ user_friendly_bot/training.py
@@ -10,7 +10,7 @@ from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense,Activation,Dropout
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import Adam
 
 lemmatizer = WordNetLemmatizer()
 
@@ -71,7 +71,12 @@ model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]),activation='softmax'))
 
 
-sgd = SGD(lr=0.01,decay=1e-6,momentum=0.9,nesterov=True)
+adam = Adam(learning_rate=0.001,
+    beta_1=0.9,
+    beta_2=0.999,
+    epsilon=1e-07,
+    amsgrad=False,
+    name="Adam")
 model.compile(loss='categorical_crossentropy',optimizer=sgd,metrics=['accuracy'])
 
 hist = model.fit(np.array(train_x),np.array(train_y),epochs=200,batch_size=5,verbose=1)
